1647cdf798292d973cddd08a091af6c55091be9b
DvitryG <dmitry135531@gmail.com>
2021-03-16 12:50:47 +0300

    убрал все лишнее, создал и настроил базовые станички сайта

---
+++ Module6Project/Pages/algorithms/GeneticAlgorithm.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Module6Project.Pages.algorithms
+{
+    public class GeneticAlgorithmModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
