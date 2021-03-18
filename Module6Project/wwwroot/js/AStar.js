eb8a987e26b00e5eb5052830a299df2e846bddfb
DvitryG <dmitry135531@gmail.com>
2021-03-18 12:18:06 +0300

    добавил возможность маштабирования границ + небольшие улучшения

--- Module6Project/wwwroot/js/AStar.js
+++ Module6Project/wwwroot/js/AStar.js
@@ -1,8 +1,9 @@
 ﻿class render {
-    constructor(canvas_name, cell_size, num_cells) {
+    constructor(canvas_name, cell_size, num_cells, cell_border) {
         this.canvas_name = canvas_name;
         this.cell_size = cell_size;
         this.num_cells = num_cells;
+        this.cell_border = cell_border;
 
         var canvas = document.getElementById(canvas_name);
         canvas.height = cell_size * num_cells;
@@ -13,56 +14,51 @@
         this.context.webkitImageSmoothingEnabled = false;
         this.context.msImageSmoothingEnabled = false;
         this.context.imageSmoothingEnabled = false;
+    }
 
-        this.drawGrid(cell_size, num_cells);
-
+    drawGrid(color) {
+        if (color == color.match(/#\w{1,8}/)) {
+            this.context.strokeStyle = color;
+        }
+        else return;
+        if (this.cell_border == 0) return;
+        else this.context.lineWidth = this.cell_border + (this.cell_border - 1);
+        this.context.strokeRect(0.5, 0.5, this.cell_size * this.num_cells - 1, this.cell_size * this.num_cells - 1);
+        for (var x = 0.5; x <= this.cell_size * this.num_cells; x += this.cell_size) {
+            this.context.moveTo(x, 0.5);
+            this.context.lineTo(x, this.cell_size * this.num_cells - 1);
+            this.context.moveTo(x + (this.cell_size - 1), 0.5);
+            this.context.lineTo(x + (this.cell_size - 1), this.cell_size * this.num_cells - 1);
+        }
+        for (var y = 0.5; y <= this.cell_size * this.num_cells; y += this.cell_size) {
+            this.context.moveTo(0.5, y);
+            this.context.lineTo(this.cell_size * this.num_cells - 1, y);
+            this.context.moveTo(0.5, y + (this.cell_size - 1));
+            this.context.lineTo(this.cell_size * this.num_cells - 1, y + (this.cell_size - 1));
+        }
         this.context.stroke();
     }
 
-    drawGrid(cell_size, num_cells) {
-        this.context.strokeStyle = "#888";
-        this.context.strokeRect(0.5, 0.5, cell_size * num_cells - 1, cell_size * num_cells - 1);
-        for (var x = 0.5; x <= cell_size * num_cells; x += cell_size) {
-            this.context.moveTo(x, 0.5);
-            this.context.lineTo(x, cell_size * num_cells - 1);
-            this.context.moveTo(x + (cell_size - 1), 0.5);
-            this.context.lineTo(x + (cell_size - 1), cell_size * num_cells - 1);
-        }
-        for (var y = 0.5; y <= cell_size * num_cells; y += cell_size) {
-            this.context.moveTo(0.5, y);
-            this.context.lineTo(cell_size * num_cells - 1, y);
-            this.context.moveTo(0.5, y + (cell_size - 1));
-            this.context.lineTo(cell_size * num_cells - 1, y + (cell_size - 1));
-        }
-    }
-
     decorateCell(x, y, decor) {
+        var border = this.cell_border
         if (decor == "None") {
-            this.context.clearRect(this.cell_size * x + 1, this.cell_size * y + 1, this.cell_size - 2, this.cell_size - 2);
+            this.context.clearRect(this.cell_size * x + this.cell_border, this.cell_size * y + this.cell_border, this.cell_size - 2 * this.cell_border, this.cell_size - 2 * this.cell_border);
         }
         else {
-            if (decor == decor.match(/#\w{1,6}/)) {
+            if (decor == decor.match(/#\w{1,8}/)) {
                 this.context.fillStyle = decor;
-                this.context.fillRect(this.cell_size * x + 1, this.cell_size * y + 1, this.cell_size - 2, this.cell_size - 2)
+                this.context.fillRect(this.cell_size * x + this.cell_border, this.cell_size * y + this.cell_border, this.cell_size - 2 * this.cell_border, this.cell_size - 2 * this.cell_border)
             }
             else {
-                /*Тут должна быть реализована вставка картинок*/
+            /*Тут должна быть реализована вставка картинок*/
             }
         }
     }
 }
 
 function init() {
-    var canvas = new render("canvas", 40, 7);
-    canvas.decorateCell(3, 2, "#f00");
-    canvas.decorateCell(3, 3, "#f00");
-    canvas.decorateCell(5, 2, "#f00");
-    canvas.decorateCell(5, 3, "#f00");
-    canvas.decorateCell(2, 5, "#f00");
-    canvas.decorateCell(3, 6, "#f00");
-    canvas.decorateCell(4, 6, "#f00");
-    canvas.decorateCell(5, 6, "#f00");
-    canvas.decorateCell(6, 5, "#f00");
+    var canvas = new render("canvas", 4, 10, 1);
+    canvas.drawGrid("#000");
 }
 
 window.onload = init;
