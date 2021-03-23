794992aa82439022bc0938e3c1b9ab765b4b82ef
DvitryG <dmitry135531@gmail.com>
2021-03-23 12:18:33 +0300

    начал реализовывать интерфейс первого алгорима, изменил методы включения/отключения рисования мышкой

--- Module6Project/wwwroot/js/AStar.js
+++ Module6Project/wwwroot/js/AStar.js
@@ -1,10 +1,44 @@
 ﻿"use strict";
-import {Canvas} from "./engine/render.js"
+import { Canvas } from "./engine/render.js";
+
+function colors(canvas) {
+    canvas.mouseDraw(0, "None");
+    var colors = document.getElementById("colors").addEventListener('change', event => {
+        for (var opt of event.target.children) {
+            if (opt.selected) {
+                if (opt.value == 0) canvas.mouseDraw(opt.value, "None");
+                else if (opt.value == 1) canvas.mouseDraw(opt.value, "#000");
+                else if (opt.value == 2) canvas.mouseDraw(opt.value, "#f2f");
+                else if (opt.value == 3) canvas.mouseDraw(opt.value, "#ff3");
+                else console.log(`Неизвесное значение: ${opt.value}`);
+            }
+        }
+    });
+}
+
+function generators(canvas) {
+    var generators = document.getElementById("generators").addEventListener('change', event => {
+        for (var opt of event.target.children) {
+            if (opt.selected) {
+            /*none*/
+            }
+        }
+    });
+}
+
+function start(canvas) {
+    var start = document.getElementById("start").addEventListener('click', event => {
+        canvas.setMouseDraw(false);
+    });
+}
 
 function init() {
     var canvas = new Canvas("canvas", 500, 40, 50, 1);
     canvas.drawGrid("#000");
-    canvas.onMouseDraw(1, "#f00");
+    colors(canvas);
+    generators(canvas);
+    start(canvas);
+
 }
 
 window.onload = init;
--- Module6Project/wwwroot/js/engine/render.js
+++ Module6Project/wwwroot/js/engine/render.js
@@ -1,11 +1,12 @@
 ﻿export class Canvas {
     constructor(canvas_name, canvas_size, cell_size, num_cells, cell_border) {
         //this._canvas_name = canvas_name;
-        this._canvas_size = canvas_size;
-        this._cell_size = cell_size;
-        this._num_cells = num_cells;
-        this._cell_border = cell_border;
+        this._canvasSize = canvas_size;
+        this._cellSize = cell_size;
+        this._numCells = num_cells;
+        this._cellBorder = cell_border;
         this._values = new Array(num_cells);
+        this._mouseFlag = false;
         for (var i = 0; i < this._values.length; ++i) {
             this._values[i] = new Array(num_cells);
             for (var j = 0; j < this._values.length; ++j) {
@@ -32,20 +33,20 @@
             this.context.strokeStyle = color;
         }
         else return;
-        if (this._cell_border == 0) return;
-        else this.context.lineWidth = this._cell_border + (this._cell_border - 1);
-        this.context.strokeRect(0.5, 0.5, this._cell_size * this._num_cells - 1, this._cell_size * this._num_cells - 1);
-        for (var x = 0.5; x <= this._cell_size * this._num_cells; x += this._cell_size) {
+        if (this._cellBorder == 0) return;
+        else this.context.lineWidth = this._cellBorder + (this._cellBorder - 1);
+        this.context.strokeRect(0.5, 0.5, this._cellSize * this._numCells - 1, this._cellSize * this._numCells - 1);
+        for (var x = 0.5; x <= this._cellSize * this._numCells; x += this._cellSize) {
             this.context.moveTo(x, 0.5);
-            this.context.lineTo(x, this._cell_size * this._num_cells - 1);
-            this.context.moveTo(x + (this._cell_size - 1), 0.5);
-            this.context.lineTo(x + (this._cell_size - 1), this._cell_size * this._num_cells - 1);
+            this.context.lineTo(x, this._cellSize * this._numCells - 1);
+            this.context.moveTo(x + (this._cellSize - 1), 0.5);
+            this.context.lineTo(x + (this._cellSize - 1), this._cellSize * this._numCells - 1);
         }
-        for (var y = 0.5; y <= this._cell_size * this._num_cells; y += this._cell_size) {
+        for (var y = 0.5; y <= this._cellSize * this._numCells; y += this._cellSize) {
             this.context.moveTo(0.5, y);
-            this.context.lineTo(this._cell_size * this._num_cells - 1, y);
-            this.context.moveTo(0.5, y + (this._cell_size - 1));
-            this.context.lineTo(this._cell_size * this._num_cells - 1, y + (this._cell_size - 1));
+            this.context.lineTo(this._cellSize * this._numCells - 1, y);
+            this.context.moveTo(0.5, y + (this._cellSize - 1));
+            this.context.lineTo(this._cellSize * this._numCells - 1, y + (this._cellSize - 1));
         }
         this.context.stroke();
     }
@@ -53,12 +54,12 @@
     decorateCell(x, y, value, decor) {
         this._values[y][x] = value;
         if (decor == "None") {
-            this.context.clearRect(this._cell_size * x + this._cell_border, this._cell_size * y + this._cell_border, this._cell_size - 2 * this._cell_border, this._cell_size - 2 * this._cell_border);
+            this.context.clearRect(this._cellSize * x + this._cellBorder, this._cellSize * y + this._cellBorder, this._cellSize - 2 * this._cellBorder, this._cellSize - 2 * this._cellBorder);
         }
         else {
             if (decor == decor.match(/#\w{1,8}/)) {
                 this.context.fillStyle = decor;
-                this.context.fillRect(this._cell_size * x + this._cell_border, this._cell_size * y + this._cell_border, this._cell_size - 2 * this._cell_border, this._cell_size - 2 * this._cell_border)
+                this.context.fillRect(this._cellSize * x + this._cellBorder, this._cellSize * y + this._cellBorder, this._cellSize - 2 * this._cellBorder, this._cellSize - 2 * this._cellBorder)
             }
             else {
                 /*Тут должна быть реализована вставка картинок*/
@@ -67,21 +68,30 @@
     }
 
     _onMouse(e, value, decor) {
-        var x = Math.floor((e.offsetX * this._num_cells) / this._canvas_size);
-        var y = Math.floor((e.offsetY * this._num_cells) / this._canvas_size);
+        var x = Math.floor((e.offsetX * this._numCells) / this._canvasSize);
+        var y = Math.floor((e.offsetY * this._numCells) / this._canvasSize);
         if (e.buttons > 0) {
             //console.log(`x: ${x}  y: ${y}`);
             this.decorateCell(x, y, value, decor);
         }
     }
 
-    onMouseDraw(value, decor) {
-        this._canvas.onmousemove = (e) => this._onMouse(e, value, decor);
-        this._canvas.onmousedown = (e) => this._onMouse(e, value, decor);
+    setMouseDraw(value) {
+        this._mouseFlag = value;
     }
 
-    offMouseDraw() {
-        this._canvas.onmousemove = null;
-        this._canvas.onmousedown = null;
+    getMouseDraw() {
+        return this._mouseFlag;
+    }
+
+    mouseDraw(value, decor) {
+        if (this._mouseFlag) {
+            this._canvas.onmousemove = (e) => this._onMouse(e, value, decor);
+            this._canvas.onmousedown = (e) => this._onMouse(e, value, decor);
+        }
+        else {
+            this._canvas.onmousemove = null;
+            this._canvas.onmousedown = null;
+        }
     }
 }
