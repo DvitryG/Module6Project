048e7ee8765dc924b06789324615952f48f8cd15
DvitryG <dmitry135531@gmail.com>
2021-03-25 18:36:32 +0300

    временный и не рабочий вариант A*

--- Module6Project/wwwroot/js/AStar.js
+++ Module6Project/wwwroot/js/AStar.js
@@ -1,49 +1,114 @@
 ﻿"use strict";
 import { Canvas, Point } from "./engine/render.js";
 
-function colors(canvas) {
-    var start = new Point(canvas, 2, "#f0f");
-    var finish = new Point(canvas, 2, "#0ff");
+class Program {
+    constructor() {
+        this.canvas = new Canvas("canvas", 500, 40, 50, 1);
+        this.canvas.drawGrid("#000");
+        this.canvas.setMouseDraw(true);
+        this.start = new Point(this.canvas, 2, "#f0f");
+        this.finish = new Point(this.canvas, 3, "#0ff");
+    }
 
-    canvas.mouseDraw(0, "void");
-    var colors = document.getElementById("colors").addEventListener('change', event => {
-        for (var opt of event.target.children) {
-            if (opt.selected) {
-                if (opt.value == 0) canvas.mouseDraw(0, "void");
-                else if (opt.value == 1) canvas.mouseDraw(opt.value, "#000");
-                else if (opt.value == 2) start.drawPoint();
-                else if (opt.value == 3) finish.drawPoint();
-                else console.log(`Неизвесное значение: ${opt.value}`);
+    AStar() {
+        var graph = new Array(this.canvas._values.length);
+        for (var i = 0; i < graph.length; ++i) {
+            graph[i] = new Array(graph.length);
+            for (var j = 0; j < graph.length; ++j) {
+                graph[i][j] = {
+                    g: 0, h: 0, f: 0,
+                    x: i, y: j,
+                    visited: false,
+                    closed: false,
+                    parent: null,
+                }
             }
         }
-    });
-}
 
-function generators(canvas) {
-    var generators = document.getElementById("generators").addEventListener('change', event => {
-        for (var opt of event.target.children) {
-            if (opt.selected) {
-            /*none*/
+        var finish = graph[this.finish.lastX][this.finish.lastY];
+        var open = [];
+        //var closed = [];
+        open.push(graph[this.start.lastX][this.start.lastY]);
+        while (open.length > 0) {
+            var min = 0;
+            for (var i = 0; i < open.length; ++i) {
+                if (open[i].f < open[min].f) min = i;
             }
+            var current = open[min];
+            if (current == finish) {
+                var path = [];
+                var curr = current
+                while (curr.parent) {
+                    path.push(curr);
+                    curr = curr.parent;
+                }
+                return path;
+            }
+            current.closed = true;
+            for (var v in { a: [1, 0], b:[0, 1], c: [-1, 0], d: [0, -1]}) {
+                console.log(`${v[0]} ${v[1]}`);
+                if (this.canvas._values[current.x + v[0]][current.y + v[1]] != 1 && !graph[current.x + v[0]][current.y + v[1]].closed) {
+                    var g = current.g + 1;
+                    var h = Math.abs(graph[current.x + v[0]][current.y + v[1]].x - this.finish.lastX) + Math.abs(graph[current.x + v[0]][current.y + v[1]].y - this.finish.lastY);
+                    var f = g + h;
+                    if (!graph[current.x + v[0]][current.y + v[1]].visited) open.push(graph[current.x + v[0]][current.y + v[1]]);
+                    if (!graph[current.x + v[0]][current.y + v[1]].visited || f < graph[current.x + v[0]][current.y + v[1]].f) {
+                        graph[current.x + v[0]][current.y + v[1]].g = g;
+                        graph[current.x + v[0]][current.y + v[1]].h = h;
+                        graph[current.x + v[0]][current.y + v[1]].f = f;
+                        graph[current.x + v[0]][current.y + v[1]].visited = true;
+                        graph[current.x + v[0]][current.y + v[1]].parent = current;
+                    }
+                }
+            }
+            open.splice(open.indexOf(min), 1);
+            //closed.push(current);
         }
-    });
+        return false;
+    }
+
+    colors() {
+        this.canvas.mouseDraw(0, "void");
+        var colors = document.getElementById("colors").addEventListener('change', event => {
+            for (var opt of event.target.children) {
+                if (opt.selected) {
+                    if (opt.value == 0) this.canvas.mouseDraw(0, "void");
+                    else if (opt.value == 1) this.canvas.mouseDraw(1, "#000");
+                    else if (opt.value == 2) this.start.drawPoint();
+                    else if (opt.value == 3) this.finish.drawPoint();
+                    else console.log(`Неизвесное значение: ${opt.value}`);
+                }
+            }
+        });
+    }
+
+    generators() {
+        var generators = document.getElementById("generators").addEventListener('change', event => {
+            for (var opt of event.target.children) {
+                if (opt.selected) {
+                    /*none*/
+                }
+            }
+        });
+    }
+
+    startButton() {
+        var start = document.getElementById("start").addEventListener('click', event => {
+            this.canvas.setMouseDraw(false);
+            way = this.AStar();
+            if (way != false) {
+                n = way.pop();
+                this.canvas.decorateCell(n.x, n.y, 0, "#fff");
+            }
+        });
+    }
 }
-
-function startButton(canvas) {
-    var start = document.getElementById("start").addEventListener('click', event => {
-        canvas.setMouseDraw(false);
-    });
-}
-
-
 
 function init() {
-    var canvas = new Canvas("canvas", 500, 40, 50, 1);
-    canvas.drawGrid("#000");
-    canvas.setMouseDraw(true);
-    colors(canvas);
-    generators(canvas);
-    startButton(canvas);
+    var prog = new Program();
+    prog.colors();
+    prog.generators();
+    prog.startButton();
 
 }
 
