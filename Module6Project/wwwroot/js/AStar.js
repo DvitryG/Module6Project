d70a5ed7b37111b57f6a0788e80c4772b53d09a9
DvitryG <dmitry135531@gmail.com>
2021-04-06 18:12:03 +0300

    Добавил новый класс работы с графикой, эффект глитчинга текста + еще много чего по мелочи

--- Module6Project/wwwroot/js/AStar.js
+++ Module6Project/wwwroot/js/AStar.js
@@ -1,5 +1,5 @@
 ﻿"use strict";
-import { Canvas, Point } from "./engine/render.js";
+import { DiscreteArea, Point } from "./engine/render.js";
 
 class Program {
     constructor() {
@@ -12,7 +12,6 @@ class Program {
     }
 
     AStar() {
-        //console.log(`Да начнется веселье!`);
         var graph = new Array(this.canvas._values.length);
         for (var i = 0; i < graph.length; ++i) {
             graph[i] = new Array(graph.length);
@@ -24,75 +23,69 @@ class Program {
                     closed: false,
                     parent: null,
                 }
-                ////console.log(`${graph[i][j].x}`);
             }
         }
-        //console.log(`Граф инициализирован`);
 
         function adjacents(current) {
             var adjacent = [];
             var x = current.x;
             var y = current.y;
-            //console.log(`родительская клетка: x: ${x}  y: ${y}`);
             if ((x - 1 >= 0 && x - 1 < graph.length) && (y >= 0 && y < graph.length)) {
-                ////console.log(`${canvas._values[x - 1][y]}`);
                 adjacent.push(graph[x - 1][y]);
-                //console.log(`добавляю соседнюю в очередь: x: ${x - 1}  y: ${y}`);
             }
             if ((x >= 0 && x < graph.length) && (y + 1 >= 0 && y + 1 < graph.length)) {
                 adjacent.push(graph[x][y + 1]);
-                //console.log(`добавляю соседнюю в очередь: x: ${x}  y: ${y + 1}`);
             }
             if ((x + 1 >= 0 && x + 1 < graph.length) && (y >= 0 && y < graph.length)) {
                 adjacent.push(graph[x + 1][y]);
-                //console.log(`добавляю соседнюю в очередь: x: ${x + 1}  y: ${y}`);
             }
             if ((x >= 0 && x < graph.length) && (y - 1 >= 0 && y - 1 < graph.length)) {
                 adjacent.push(graph[x][y - 1]);
-                //console.log(`добавляю соседнюю в очередь: x: ${x}  y: ${y - 1}`);
             }
-            //console.log(`Получаем соседние клетки: ${adjacent}`);
             return adjacent;
         }
 
         var finish = graph[this.finish.lastX][this.finish.lastY];
-        //console.log(`Получаем финиш, который находится на: x: ${finish.x} y: ${finish.y}`);
         var open = [];
-        //var closed = [];
         open.push(graph[this.start.lastX][this.start.lastY]);
-        //console.log(`добавяем в очередь старт, который находится на: x: ${graph[this.start.lastX][this.start.lastY].x} y: ${graph[this.start.lastX][this.start.lastY].y}`);
         while (open.length > 0) {
-            //console.log(`достаем из открытого списка значение.`);
             var min = 0;
+            //console.log(`Ищем минимальный из:`);
             for (var i = 0; i < open.length; ++i) {
+                //console.log(`   x: ${open[i].x}  y: ${open[i].y} f: ${open[i].f}`);
                 if (open[i].f < open[min].f && !open[i].closed) min = i;
+                else if (open[i].f == open[min].f && !open[i].closed) {
+                    if (open[i].h < open[min].h) min = i;
+                }
             }
             var current = open[min];
-            //console.log(`находим самое маленькое из списка: x: ${current.x} y: ${current.y}`);
+            //console.log(`Итог: x: ${current.x}  y: ${current.y} f: ${current.f}`);
             if (current == finish) {
-                //console.log(`О УРА, оно подходит!`);
                 var path = [];
                 var curr = current
                 while (curr.parent) {
                     path.push(curr);
                     curr = curr.parent;
                 }
-                //console.log(`Путь: ${path}`);
                 return path;
             }
+            
+            this.canvas.decorateCell(current.x, current.y, 0, "#00f");
+
             current.closed = true;
             var adjacent = adjacents(current);
-            //console.log(`получаем список соседних: ${adjacent}`);
+            //this.canvas.decorateCell(open[min].x, open[min].y, 0, "void");
+            open.splice(min, 1);
             for (var i in adjacent) {
-                //console.log(`производим анализ для соседней: x: ${adjacent[i].x} y: ${adjacent[i].y}`);
-                //console.log(`стенка? ${this.canvas._values[adjacent[i].x][adjacent[i].y]} закрыта? ${adjacent[i].closed}`);
                 if (this.canvas._values[adjacent[i].x][adjacent[i].y] != 1 && !adjacent[i].closed) {
-                    //console.log(`Хорошие новости: это не стенка и оно не в закрытом`);
-                    var g = current.g + 1;
-                    var h = Math.abs(adjacent[i].x - this.finish.lastX) + Math.abs(adjacent[i].y - this.finish.lastY);
+                    var g = current.g + 0.6;
+                    var h = Math.sqrt((adjacent[i].x - this.finish.lastX) ** 2 + (adjacent[i].y - this.finish.lastY) ** 2);
                     var f = g + h;
-                    //console.log(`посещенная? ${adjacent[i].visited}`);
-                    if (!adjacent[i].visited) open.push(adjacent[i]);
+                    if (!adjacent[i].visited && !adjacent[i].closed) {
+                        //this.canvas.decorateCell(open[min].x, open[min].y, 0, "#00ff00");
+                        open.push(adjacent[i]);
+                        //console.log(`Добавил в очередь: x: ${adjacent[i].x}  y: ${adjacent[i].y} f: ${adjacent[i].f}`);
+                    }
                     if (!adjacent[i].visited || f < adjacent[i].f) {
                         adjacent[i].g = g;
                         adjacent[i].h = h;
@@ -101,16 +94,9 @@ class Program {
                         adjacent[i].parent = current;
                     }
                 }
-                //console.log(`закончили проверку для: x: ${adjacent[i].x} y: ${adjacent[i].y}`);
-                
             }
-            //console.log(`открытый список до удаления: ${open} ${open.length}`);
-            //console.log(`мы удаляем: x: ${open[min].x} y: ${open[min].y}`);
-            open.splice(min, 1);
-            //console.log(`открытый список после удаления: ${open} ${open.length}`);
-            //closed.push(current);
+            
         }
-        //console.log(`AStar: ${false}`);
         return false;
     }
 
@@ -120,9 +106,8 @@ class Program {
 
     generation() {
         var generation = document.getElementById("generate").addEventListener('click', event => {
-            //console.log(`оп, выполнилось`);
             this.num_cells = this.inputSize();
-            this.canvas = new Canvas("canvas", 500, 40, this.num_cells, 1);
+            this.canvas = new DiscreteArea("canvas", 600, 1, this.num_cells, 0);
             this.canvas.drawGrid("#000");
             this.canvas.setMouseDraw(true);
             this.start = new Point(this.canvas, 2, "#f0f");
@@ -159,14 +144,11 @@ class Program {
 
     startButton() {
         var start = document.getElementById("start").addEventListener('click', event => {
-            this.canvas.setMouseDraw(false);
+            //this.canvas.setMouseDraw(false);
             var way = this.AStar();
-            //console.log(way);
             if (way != false) {
                 while (way.length > 0) {
-                    //console.log(way.length);
                     var n = way.pop();
-                    //console.log(`x: ${n.x} y: ${n.y}`)
                     this.canvas.decorateCell(n.x, n.y, 0, "#ff0");
                 }
             }
@@ -176,11 +158,6 @@ class Program {
 
 function init() {
     var prog = new Program();
-    //prog.generation();
-    /*prog.colors();
-    prog.generators();
-    prog.startButton();*/
-
 }
 
 window.onload = init;
--- Module6Project/wwwroot/js/AntCO.js
+++ Module6Project/wwwroot/js/AntCO.js
@@ -1,10 +1,47 @@
 ﻿"use strict";
-import { Canvas } from "./engine/render.js"
+import { SmoothArea } from "./engine/render.js"
+
+function mouseLogic(canvas) {
+    //console.log(`задаем логику`);
+    canvas.canvas.onmousedown = (e) => {
+        var mouseX = e.offsetX;
+        var mouseY = e.offsetY;
+        var mx, my;
+        var index = null;
+        var collision = false;
+        for (var i in canvas.objects) {
+            if (canvas.pointCollision(canvas.objects[i], mouseX, mouseY)) {
+                collision = true;
+                index = i;
+                mx = mouseX - canvas.objects[i].x;
+                my = mouseY - canvas.objects[i].y;
+                break;
+            }
+        }
+        if (!collision) {
+            canvas.canvas.onmousemove = (e) => {
+                if (e.buttons > 0) {
+                    canvas.mouseSetObj(e, "circle", "#f00", true, 100, 100);
+                }
+            }
+        }
+        else {
+            canvas.canvas.onmousemove = (e) => {
+                if (e.buttons > 0) {
+                    canvas.mouseCapture(e, index, mx, my);
+                }
+            }
+        }
+    }
+}
 
 function init() {
-    var canvas = new Canvas("canvas", 500, 40, 50, 1);
-    canvas.drawGrid("#000");
-    canvas.onMouseDraw(1, "#f00");
+    var canvas = new SmoothArea("canvas", 500, 1000);
+    canvas.objBorders = true;
+    //canvas.addObject("", null, 10, 10, 100, 100);
+    canvas.addObject("circle", "#000", true, 200, 200, 100, 100);
+    mouseLogic(canvas);
+    canvas.setSpeed(10);
 }
 
 window.onload = init;
